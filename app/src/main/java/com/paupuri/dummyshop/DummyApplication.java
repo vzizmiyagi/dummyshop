5aa944d9f1d1865e168b9b13c1ede5389a701682
vzizmiyagi <aziz.ppr@gmail.com>
2017-02-01 22:32:33 +0000

    Update DummyApplication.java

--- app/src/main/java/com/paupuri/dummyshop/DummyApplication.java
+++ app/src/main/java/com/paupuri/dummyshop/DummyApplication.java
@@ -27,9 +27,9 @@ public class DummyApplication extends Application {
 
         // Add your initialization code here
         Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
-                .applicationId("d9102f39980cbb0860b3c0831f7f14ec5e1b0b23")
-                .clientKey("8f2618a529b8e561f6c37316929a396f5c89346d")
-                .server("http://ec2-54-92-154-243.compute-1.amazonaws.com:80/parse/")
+                .applicationId("API_KEY")
+                .clientKey("CLIENT_KEY")
+                .server("SERVER_ADDRESS")
                 .build()
         );
 
